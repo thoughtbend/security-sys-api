7dde347551d4cee2da53b0c31927e1c77545bbe3
Mike Nolan <michael.nolan@parivedasolutions.com>
2019-03-23 21:24:12 -0500

    Updates for AJP

--- src/main/java/com/thoughtbend/securitysysapi/SecuritySysApiApplication.java
+++ src/main/java/com/thoughtbend/securitysysapi/SecuritySysApiApplication.java
@@ -3,10 +3,13 @@ package com.thoughtbend.securitysysapi;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.apache.catalina.connector.Connector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -31,6 +34,27 @@ public class SecuritySysApiApplication {
 		};
 	}
 	
+	@Bean
+	protected ServletWebServerFactory servletContainer() {
+		
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		
+		tomcat.addAdditionalTomcatConnectors(ajpConnector());
+		
+		return tomcat;
+	}
+	
+	private Connector ajpConnector() {
+		
+		Connector connector = new Connector("AJP/1.3");
+		connector.setScheme("http");
+		connector.setPort(9004);
+		connector.setSecure(false);
+		connector.setAllowTrace(false);
+		
+		return connector;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SecuritySysApiApplication.class);
 	}
